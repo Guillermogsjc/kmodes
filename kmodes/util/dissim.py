ee72fba02906660703360a161873556663a814c2
Guillermo González <guillermogsjc@gmail.com>
2016-12-15 21:41:21 +0100

    Update dissim.py

--- kmodes/util/dissim.py
+++ kmodes/util/dissim.py
@@ -13,7 +13,7 @@ def matching_dissim(a, b):
     """Simple matching dissimilarity function"""
                
     return sk.metrics.pairwise.pairwise_distances(np.atleast_2d(a),
-                                                  np.atleast_2d(b), metric='l1', n_jobs=-1).astype(int).ravel()
+                                                  np.atleast_2d(b), metric='l1', n_jobs=1).astype(int).ravel()
 
 def euclidean_dissim(a, b):
     """Euclidean distance dissimilarity function"""
