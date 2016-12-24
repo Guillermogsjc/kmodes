1734438a1012ca77d8059e746e2ec8bd44db6db8
Guillermo González <guillermogsjc@gmail.com>
2016-12-24 11:02:16 +0100

    Update dissim.py

--- kmodes/util/dissim.py
+++ kmodes/util/dissim.py
@@ -8,12 +8,23 @@ Dissimilarity measures for clustering
 import numpy as np
 import sklearn as sk  
 
+def distchi(a,b):
+    return 2*np.sum((a-b)**2/(0.1+a+b))  
 
 def matching_dissim(a, b):
     """Simple matching dissimilarity function"""
                
-    return sk.metrics.pairwise.pairwise_distances(np.atleast_2d(a),
-                                                  np.atleast_2d(b), metric='l1', n_jobs=1).astype(int).ravel()
+    #return sk.metrics.pairwise.pairwise_distances(np.atleast_2d(a),
+    #                                              np.atleast_2d(b), metric='l1', n_jobs=1).astype(int).ravel()
+    from sklearn import metrics 
+
+    return metrics.pairwise.pairwise_distances(np.atleast_2d(a), np.atleast_2d(b), metric=distchi, n_jobs=1).ravel()
+
+# def matching_dissim(a, b):
+#     """Simple matching dissimilarity function"""
+               
+#     return sk.metrics.pairwise.pairwise_distances(np.atleast_2d(a),
+#                                                   np.atleast_2d(b), metric='l1', n_jobs=1).astype(int).ravel()
 
 def euclidean_dissim(a, b):
     """Euclidean distance dissimilarity function"""
